0d61646da009dbbe57e55c4f75aba1db7ed445d8
Jose Antonio Ramos <joseantonioramos24@gmail.com>
2020-09-21 01:41:09 +0200

    Create data.sql
    Some data for the database to have some examples

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO ACCOUNTS VALUES (null, 15000, 'EUR', 'First Account', true);
+INSERT INTO ACCOUNTS VALUES (null, 12300, 'EUR', 'Second Account', false);
+INSERT INTO ACCOUNTS VALUES (null, 6000, 'USD', 'Third Account', true);
+INSERT INTO ACCOUNTS VALUES (null, 17000, 'GBP', 'Fourth Account', false);
