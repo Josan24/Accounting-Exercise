af3ce9b20387b4fb95b470bb0b04ad6e723788cc
Jose Antonio Ramos <joseantonioramos24@gmail.com>
2020-09-21 02:57:05 +0200

    Creation of accounts
    Creation of accounts for a POST call

--- src/main/java/com/josan/accounting/controller/AccountsController.java
+++ src/main/java/com/josan/accounting/controller/AccountsController.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.josan.accounting.entity.Account;
 import com.josan.accounting.service.IAccountsService;
@@ -27,6 +31,20 @@ public class AccountsController {
 	@GetMapping("/accounts/{id}")
 	public Optional<Account> getAccounts(@PathVariable("id") int idAccount){
 		return accounts.search(idAccount);
+	}
+	
+	
+	@PostMapping("/accounts")
+	public Account save(@RequestBody Account account) {
+		
+		if(account.isTreasury() == false) {
+			if(account.getBalance() < 0) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Non treasury accounts can not have negative balance");
+			}
+		}
+		
+		accounts.save(account);
+		return account;
 	}
 
 }
